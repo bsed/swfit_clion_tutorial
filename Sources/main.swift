05a2ebd604eab5f11031f13c592c3fc36e623e8b
d0f <284178359@qq.com>
2016-04-04 21:55:54 +0800

    swift in clion

---
+++ Sources/main.swift
@@ -0,0 +1,35 @@
+//
+// Created by kelvin on 4/2/16.
+//
+
+import Foundation
+import PlayingCard
+import DeckOfPlayingCards
+
+class HelloWorld {
+
+    // Reference: https://swift.org/package-manager/#example-usage
+    func dealSomeCards() {
+        print("Allow me to deal some cards...")
+        let numberOfCards = 10
+
+        var deck = Deck.standard52CardDeck()
+        deck.shuffle()
+
+        for _ in 1...numberOfCards {
+            guard let card = deck.deal() else {
+                print("No More Cards!")
+                break
+            }
+
+            print(card)
+        }
+    }
+
+    func greet() {
+        print("Hello World")
+    }
+}
+var h = HelloWorld()
+h.greet()
+h.dealSomeCards()
